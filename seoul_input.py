df5aa06feae2275d8b09abde3ea0687ef1cc3430
donghyeon <donghyeonyi@gmail.com>
2018-10-10 05:17:24 +0900

    Add assert element shape for features and labels datasets

--- seoul_input.py
+++ seoul_input.py
@@ -95,25 +95,32 @@ def inverse_standardize(standardized_dataset, mean, stddev):
 
 def sliding_window_input_fn(features, labels, window_size, batch_size, num_epoch):
     """
-    :param features: dict of numpy array, [num_stations, time_length, num_features]
-    :param labels: dict of numpy array, [num_stations, time_length, num_features]
+    :param features: dict of numpy array, [num_stations, time_length]
+    :param labels: dict of numpy array, [num_stations, time_length]
     :param window_size: int
     :param batch_size: int
+    :param num_epoch: int
     :return: tf.data.Dataset
     """
     dataset_features = tf.data.Dataset.from_tensor_slices(features)
     dataset_labels = tf.data.Dataset.from_tensor_slices(labels)
 
-    sliding_window_func = tf.contrib.data.sliding_window_batch(window_size=window_size, stride=1)
+    sliding_window_func = tf.contrib.data.sliding_window_batch(window_size=window_size)
 
     dataset_features = dataset_features.flat_map(
         lambda x: tf.data.Dataset.from_tensor_slices(x).apply(sliding_window_func))
     dataset_labels = dataset_labels.flat_map(
         lambda x: tf.data.Dataset.from_tensor_slices(x).apply(sliding_window_func))
 
+    # Need to assert output_shapes because sliding_window_func forgets the TensorShape of its SlideDataset
+    dataset_features = dataset_features.apply(
+        tf.contrib.data.assert_element_shape({key: [window_size] for key in features}))
+    dataset_labels = dataset_labels.apply(
+        tf.contrib.data.assert_element_shape({key: [window_size] for key in labels}))
+
     dataset = tf.data.Dataset.zip((dataset_features, dataset_labels))
 
-    num_station, time_length = list(features.values())[0].shape
+    num_station, time_length = next(iter(features.values())).shape
     num_data = num_station * (time_length - window_size + 1)
 
     return dataset.shuffle(num_data).repeat(num_epoch).batch(batch_size)
